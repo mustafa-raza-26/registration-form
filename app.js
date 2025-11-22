31ca977aef8feab2fe0c89eda491f6fb22983abf
mustafa-raza-26 <mustafa778823@gmail.com>
2025-11-22 11:54:15 +0500

    Registration form

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const supabaseURL = 'https://awymejxeeylofpcjuuug.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3eW1lanhlZXlsb2ZwY2p1dXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDYxMzcsImV4cCI6MjA3ODYyMjEzN30.BpaYEB1T9CWb44anT0m3J5JbX4VpvsYC4juI4-b_1ic';
-const client = supabase.createClient(supabaseURL , supabaseKey)
-console.log(client);
-
+const client = supabase.createClient(supabaseURL , supabaseKey);
 
 let userName = document.getElementById('userName');
 let fatherName = document.getElementById('fatherName');
 let email = document.getElementById('email');
 let number = document.getElementById('number');
+let cnic = document.getElementById('cnic');
 let dob = document.getElementById('dob');
 let qualification = document.getElementById('qualification');
 let course = document.getElementById('course');
 let address = document.getElementById('address');
 let submit = document.getElementById('submit');
 
-
-submit.addEventListener('click', async (e)=> {
-    e.preventDefault()
-
+if (submit) {
+submit.addEventListener('click', async ()=> {
     let genderInput = document.querySelector('input[name="gender"]:checked');
     let gender = genderInput ? genderInput.value : "Not Selected";
-    console.log(gender);
-    
+
     const { data, error } = await client
     .from('Form')
     .insert({
@@ -29,6 +25,7 @@ submit.addEventListener('click', async (e)=> {
         father_name : fatherName.value,
         email : email.value,
         number : number.value,
+        cnic : cnic.value,
         d_o_b : dob.value,
         gender : gender,
         course : course.value,
@@ -39,5 +36,13 @@ submit.addEventListener('click', async (e)=> {
 
     if (error) {
         alert('error' + error.message)
+        return
     }
+
+     alert("Form Submitted Successfully!");
+
+    document.querySelectorAll("input").forEach(inp => inp.value = "");
+    document.querySelectorAll("select").forEach(sel => sel.value = "");
+    address.value = "";
 })
+}
